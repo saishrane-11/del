749c57190d1642d2857965500ae5aaf181d1fe0d
saishrane-11 <saishrane20@gmail.com>
2024-01-19 12:48:51 +0530

    'hi'

---
+++ fireConfig.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBe_qFxXpc047l_1-6SQzqwBkgfHbU6KAc",
+    authDomain: "myfireprowithcrud-a5e0b.firebaseapp.com",
+    databaseURL: "https://myfireprowithcrud-a5e0b-default-rtdb.firebaseio.com",
+    projectId: "myfireprowithcrud-a5e0b",
+    storageBucket: "myfireprowithcrud-a5e0b.appspot.com",
+    messagingSenderId: "764933330659",
+    appId: "1:764933330659:web:4663b99f936b85c10a97d6"
+  };
+  
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
